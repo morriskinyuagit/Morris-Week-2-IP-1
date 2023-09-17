3b7aae9b9da79e8cd2c7840d19e1f133eba02dfd
morriskinyuagit <morris.kinyua.git@gmail.com>
2023-09-17 22:34:11 +0800

    Changed the node modules

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://morris:nIabBAC7JpCbpqfm@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://morris:nIabBAC7JpCbpqfm@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://morris:nIabBAC7JpCbpqfm@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://morris:nIabBAC7JpCbpqfm@cluster0.bztw5rc.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://morris:nIabBAC7JpCbpqfm@cluster0.bztw5rc.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://morris:nIabBAC7JpCbpqfm@cluster0.bztw5rc.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
